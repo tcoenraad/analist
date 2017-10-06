54513ba93715a5c862c2cf8a73e66b0113a08f22
Twan Coenraad <twan@twancoenraad.nl>
2017-10-06 14:07:37 +0200

    Rename annotation_node to annotated_node

--- spec/analist/annotator_spec.rb
+++ spec/analist/annotator_spec.rb
@@ -5,46 +5,46 @@ RSpec.describe Analist::Annotator do
 
   describe '#annotate' do
     context 'when parsing a simple calculation' do
-      subject(:annotation_node) { described_class.annotate(CommonHelpers.parse('1 + 1')) }
+      subject(:annotated_node) { described_class.annotate(CommonHelpers.parse('1 + 1')) }
 
-      it { expect(annotation_node.annotation).to eq [Integer, [Integer], Integer] }
+      it { expect(annotated_node.annotation).to eq [Integer, [Integer], Integer] }
     end
 
     context 'when parsing a simple method call' do
-      subject(:annotation_node) { described_class.annotate(CommonHelpers.parse('"word".reverse')) }
+      subject(:annotated_node) { described_class.annotate(CommonHelpers.parse('"word".reverse')) }
 
-      it { expect(annotation_node.annotation).to eq [String, [], String] }
+      it { expect(annotated_node.annotation).to eq [String, [], String] }
     end
 
     context 'when parsing a chained method call' do
-      subject(:annotation_node) do
+      subject(:annotated_node) do
         described_class.annotate(CommonHelpers.parse('"word".reverse.upcase'))
       end
 
-      it { expect(annotation_node).to be_instance_of Analist::AnnotatedNode }
-      it { expect(annotation_node.annotation).to eq [String, [], String] }
-      it { expect(annotation_node.children.first.annotation).to eq [String, [], String] }
+      it { expect(annotated_node).to be_instance_of Analist::AnnotatedNode }
+      it { expect(annotated_node.annotation).to eq [String, [], String] }
+      it { expect(annotated_node.children.first.annotation).to eq [String, [], String] }
     end
 
     context 'when parsing an Active Record object its property' do
-      let(:annotation_node) do
+      let(:annotated_node) do
         described_class.annotate(CommonHelpers.parse('User.first.id'), schema)
       end
-      let(:annotation_node2) do
+      let(:annotated_node2) do
         described_class.annotate(CommonHelpers.parse('User.first.first_name'), schema)
       end
 
-      it { expect(annotation_node.annotation).to eq [{ type: :User, on: :instance }, [], Integer] }
-      it { expect(annotation_node2.annotation).to eq [{ type: :User, on: :instance }, [], String] }
+      it { expect(annotated_node.annotation).to eq [{ type: :User, on: :instance }, [], Integer] }
+      it { expect(annotated_node2.annotation).to eq [{ type: :User, on: :instance }, [], String] }
     end
 
     context 'when parsing an Active Record collection' do
-      let(:annotation_node) do
+      let(:annotated_node) do
         described_class.annotate(CommonHelpers.parse('User.all'), schema)
       end
 
       it do
-        expect(annotation_node.annotation).to eq [
+        expect(annotated_node.annotation).to eq [
           { type: :User, on: :collection }, [],
           { type: :User, on: :collection }
         ]
@@ -52,20 +52,23 @@ RSpec.describe Analist::Annotator do
     end
 
     context 'when duck typing, e.g. `reverse`' do
-      let(:annotation_node) { described_class.annotate(CommonHelpers.parse('"word".reverse')) }
-      let(:annotation_node2) { described_class.annotate(CommonHelpers.parse('[1, 2, 3].reverse')) }
+      let(:annotated_node) { described_class.annotate(CommonHelpers.parse('"word".reverse')) }
+      let(:annotated_node2) { described_class.annotate(CommonHelpers.parse('[1, 2, 3].reverse')) }
 
-      it { expect(annotation_node.annotation).to eq [String, [], String] }
-      it { expect(annotation_node2.annotation).to eq [Array, [], Array] }
+      it { expect(annotated_node.annotation).to eq [String, [], String] }
+      it { expect(annotated_node2.annotation).to eq [Array, [], Array] }
     end
 
     context 'when annotating nested statements' do
-      subject(:annotation_node) { described_class.annotate(CommonHelpers.parse('[1] + ["a"]')) }
+      subject(:annotated_node) { described_class.annotate(CommonHelpers.parse('[1] + ["a"]')) }
 
-      it { expect(annotation_node.annotation).to eq [Array, [Array], Array] }
-      it { expect(annotation_node.children.first.annotation).to eq [nil, [], Array] }
+      it { expect(annotated_node.annotation).to eq [Array, [Array], Array] }
+      it { expect(annotated_node.children.first.annotation).to eq [nil, [], Array] }
       it do
-        expect(annotation_node.children.first.children.first.annotation).to eq [nil, [], Integer]
+        expect(annotated_node.children[0].children.first.annotation).to eq [nil, [], Integer]
+      end
+      it do
+        expect(annotated_node.children[2].children.first.annotation).to eq [nil, [], String]
       end
     end
   end
