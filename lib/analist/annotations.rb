71e3ce93a597f627ecfe3c39adda496491acb4b6
Twan Coenraad <tcoenraad@users.noreply.github.com>
2017-10-11 15:51:51 +0200

    Add annotation class (#2)
    * Add annotation class
    
    * Refactor things

--- lib/analist/annotations.rb
+++ lib/analist/annotations.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
 module Analist
+  class Annotation
+    attr_reader :receiver_type, :args_types, :return_type
+
+    def initialize(receiver_type, args_types, return_type)
+      @receiver_type = receiver_type.is_a?(Hash) ? receiver_type : { type: receiver_type }
+      @args_types = args_types
+      @return_type = return_type.is_a?(Hash) ? return_type : { type: return_type }
+    end
+
+    def ==(other)
+      return false unless other.is_a?(self.class)
+      [receiver_type, args_types, return_type] ==
+        [other.receiver_type, other.args_types, other.return_type]
+    end
+
+    def to_s
+      [receiver_type, args_types, return_type].to_s
+    end
+  end
+
+  class AnnotationTypeUnknown; end
+
   module Annotations
     module_function
 
@@ -8,40 +30,40 @@ module Analist
       {
         :+ => lambda do |annotated_children|
           {
-            Integer => [Integer, [Integer], Integer],
-            String => [String, [String], String],
-            Array => [Array, [Array], Array]
-          }[annotated_children.first.annotation.last]
+            Integer => Annotation.new(Integer, [Integer], Integer),
+            String => Annotation.new(String, [String], String),
+            Array => Annotation.new(Array, [Array], Array)
+          }[annotated_children.first.annotation.return_type[:type]]
         end,
-        upcase: ->(_) { [String, [], String] },
+        upcase: ->(_) { Annotation.new(String, [], String) },
         reverse: lambda do |annotated_children|
           {
-            String => [String, [], String],
-            Array => [Array, [], Array]
-          }[annotated_children.first.annotation.last]
+            String => Annotation.new(String, [], String),
+            Array => Annotation.new(Array, [], Array)
+          }[annotated_children.first.annotation.return_type[:type]]
         end,
         all: lambda do |annotated_children|
-          [
-            { type: annotated_children.first.annotation.last[:type], on: :collection },
+          Annotation.new(
+            { type: annotated_children.first.annotation.return_type[:type], on: :collection },
             [],
-            { type: annotated_children.first.annotation.last[:type], on: :collection }
-          ]
+            type: annotated_children.first.annotation.return_type[:type], on: :collection
+          )
         end,
         first: lambda do |annotated_children|
-          [
-            { type: annotated_children.first.annotation.last[:type], on: :collection },
+          Annotation.new(
+            { type: annotated_children.first.annotation.return_type[:type], on: :collection },
             [],
-            { type: annotated_children.first.annotation.last[:type], on: :instance }
-          ]
+            type: annotated_children.first.annotation.return_type[:type], on: :instance
+          )
         end
       }
     end
 
     def primitive_annotations
       {
-        int: ->(_) { [nil, [], Integer] },
-        str: ->(_) { [nil, [], String] },
-        const: ->(node) { [nil, [], type: node.children.last, on: :collection] }
+        int: ->(_) { Annotation.new(nil, [], Integer) },
+        str: ->(_) { Annotation.new(nil, [], String) },
+        const: ->(node) { Annotation.new(nil, [], type: node.children.last, on: :collection) }
       }
     end
   end
