05fb1c28c407876a1854556ad9aa711dcd23e797
Twan Coenraad <twan@twancoenraad.nl>
2017-11-08 14:13:51 +0100

    Fix minor error and version bump to v0.1.10

--- lib/analist/checker.rb
+++ lib/analist/checker.rb
@@ -7,7 +7,7 @@ module Analist
     module_function
 
     def check(node) # rubocop:disable Metrics/CyclomaticComplexity
-      return unless node.respond_to?(:type)
+      return [] unless node.respond_to?(:type)
 
       case node.type
       when :begin
--- lib/analist/version.rb
+++ lib/analist/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Analist
-  VERSION = '0.1.9.alpha'
+  VERSION = '0.1.10.alpha'
 end
