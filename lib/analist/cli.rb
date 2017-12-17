9d19a1a10aacf379d01aa0907d4f2a5790ad4a2b
Twan Coenraad <twan@twancoenraad.nl>
2017-12-17 11:32:30 +0100

    Add stdin support

--- lib/analist/cli.rb
+++ lib/analist/cli.rb
@@ -2,6 +2,7 @@
 
 require 'colorize'
 require 'optparse'
+require 'tempfile'
 
 require 'analist'
 require 'analist/text_helper'
@@ -24,6 +25,10 @@ module Analist
         parser.on('-c', '--config FILE') do |file|
           @options[:config] = file
         end
+
+        parser.on('-i', '--stdin FILE') do |file|
+          @options[:stdin] = file
+        end
       end.parse!
 
       @options[:files] = ARGV
@@ -37,13 +42,27 @@ module Analist
     end
 
     def run # rubocop:disable Metrics/AbcSize
-      puts "Inspecting #{pluralize(files.size, 'file')}"
+      if @options[:stdin]
+        file = Tempfile.new([File.basename(@options[:stdin], '.*'), File.extname(@options[:stdin])])
+        begin
+          file.write(ARGF.read)
+          file.rewind
+          @options[:files] = [file.path]
+          print_collected_errors
+          error_count = 1
+        ensure
+          file.close
+          file.unlink
+        end
+      else
+        puts "Inspecting #{pluralize(files.size, 'file')}"
 
-      print_collected_errors
+        print_collected_errors
 
-      error_count = collected_errors.inject(0) { |sum, e| sum + e.count }
-      puts "#{pluralize(files.size, 'file')} inspected, "\
-        "#{pluralize(error_count, 'error')} found"
+        error_count = collected_errors.inject(0) { |sum, e| sum + e.count }
+        puts "#{pluralize(files.size, 'file')} inspected, "\
+          "#{pluralize(error_count, 'error')} found"
+      end
 
       exit 1 if error_count.positive?
     end
